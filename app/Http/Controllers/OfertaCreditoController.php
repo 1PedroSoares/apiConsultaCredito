5d0d0236ba7f96c436e4fe4241cd5881dd84169d
PedroSoares <pedro.soaressh@gmail.com>
2024-01-24 18:54:13 -0300

    Código comentado

--- app/Http/Controllers/OfertaCreditoController.php
+++ app/Http/Controllers/OfertaCreditoController.php
@@ -2,34 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use GuzzleHttp\Client;
+use GuzzleHttp\Client;//USO A CLASSE CLIENT DO PACOTE GUZZLEHTTP
 
 class OfertaCreditoController extends Controller
 {
-   public function analiseCredito($cpf, $valorSolicitado){
+   public function analiseCredito($cpf, $valorSolicitado){ 
 
-      $valorSolicitado = (int) $valorSolicitado;
-      $client = new Client();
-      $ofertas = [];
-      $response = $client->post('https://dev.gosat.org/api/v1/simulacao/credito',[
-         'headers' => [
-            'Content-Type' => 'application/json',
+      $valorSolicitado = (int) $valorSolicitado; //CONVERTE STRING PARA INT PARA FAZER O CÁLCULO DE JUROS ANUAL
+      $client = new Client();//CRIO UMA NOVA INSTÂNCIA DA CLASSE CLIENT
+      $ofertas = []; //DECLARO UM ARRAY PARA ARMAZENAR TODOS OS OBJETOD DE OFERTAS
+      $response = $client->post('https://dev.gosat.org/api/v1/simulacao/credito',[//FAÇO UMA SOLICITAÇÃO HTTP PARA A API DA GOSAT E //ARMAZENO NA VARIÁVEL RESPONSE (QUE É A RESPOSTA DA API)
+         'headers' => [ //DEFININDO OS CABEÇALHOS DA SOLICITAÇÃO HTTP
+            'Content-Type' => 'application/json', //DEFINO O TIPO DE CONTEÚDO DA SOLICITAÇÃO HTTP COMO DADOS JSON
          ],
          'json' => [
-               "cpf" => $cpf
+               "cpf" => $cpf //DEFINO O CORPO DA SOLICITAÇÃO COMO UM ARRAY JSON E ADICIONA O CPF AO CORPO
          ]
       ])
       ->getBody()
-      ->getContents();
+      ->getContents(); //TRANSFORMO O RESPONSE EM JSON
 
-      $instituicoes = json_decode($response)->instituicoes;
+      $instituicoes = json_decode($response)->instituicoes; //TRANSFORMO O JSON EM OBJETO E PEGO AS INSTITUIÇÕES DO RESPONSE
 
-      for ($i=0; $i < count($instituicoes) ; $i++) { 
+      for ($i=0; $i < count($instituicoes) ; $i++) { //UM LAÇO DE REPETIÇÃO PARA PEGAR AS INFORMAÇÕES DOS BANCOS
          $idInstituicao = $instituicoes[$i]->id;
          $modalidades = $instituicoes[$i]->modalidades;
-
-         for ($j=0; $j < count($modalidades) ; $j++) {
+         // OUTRO LAÇO DE REPETIÇÃO PARA FAZER A REQUEST PARA PEGAR TODAS AS OFERTAS DE CRÉDITO DAS INSTITUIÇÕES
+         for ($j=0; $j < count($modalidades) ; $j++) { 
             $codigoModalidade = $modalidades[$j]->cod;
 
             $responseOferta = $client->post('https://dev.gosat.org/api/v1/simulacao/oferta',[
@@ -37,22 +36,22 @@ class OfertaCreditoController extends Controller
                   'Content-Type' => 'application/json',
                ],
                'json' => [
-                     "cpf" => $cpf,
+                     "cpf" => $cpf, //CONSULTO SE O CPF INFORMADO É VALIDO E PEGAR AS INFORMAÇÕES SOBRE ESSE CPF
                      "instituicao_id" => $idInstituicao,
                      "codModalidade" => $codigoModalidade,
                ]
             ])
             ->getBody()
             ->getContents();
-
+            
             $oferta = json_decode($responseOferta);
-            if($oferta->valorMin >= $valorSolicitado){
+            if($oferta->valorMin >= $valorSolicitado){ //DEFINO A LÓGICA DE SÓ PEGAR AS OFERTAS QUE FOR IGUAL OU MAIOR AO VALOR SOLICITADO
 
-               $taxaJurosAnual = $oferta->jurosMes * 12;
-               $taxaComposta = pow(1 + $taxaJurosAnual / 100, $oferta->QntParcelaMax);
+               $taxaJurosAnual = $oferta->jurosMes * 12;//CALCULO A TAXA DE JUROS ANUAL
+               $taxaComposta = pow(1 + $taxaJurosAnual / 100, $oferta->QntParcelaMax); //CALCULO A TAXA DE JUROS COMPOSTA
                $valorAPagar = (int) ($valorSolicitado * $taxaComposta);
 
-               array_push($ofertas, [
+               array_push($ofertas, [//EMPILHO TODAS AS VARIÁVEIS DA API DENTRO DO ARRAY DE OFERTAS
                   'instituicaoFinanceira' => $instituicoes[$i]->nome,
                   'modalidadeCredito' => $modalidades[$j]->nome,
                   'valorAPagar' => $valorAPagar,
@@ -65,7 +64,7 @@ class OfertaCreditoController extends Controller
 
       }
 
-      usort($ofertas, function($oferta1, $oferta2){
+      usort($ofertas, function($oferta1, $oferta2){//USO UMA FUNÇÃO USORT PARA ORGANIZAR AS OFERTAS PELA TAXA DE JUROS
          return $oferta1['taxaJuros'] <=> $oferta2['taxaJuros'];
       });
       return $ofertas;
